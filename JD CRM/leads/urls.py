ced28fdb179eb220fe565f95f2d93a4eb1beae23
Darshit Dudhwala <darshit1997d@gmail.com>
2021-08-29 00:00:06 +0530

    master

--- JD CRM/leads/urls.py
+++ JD CRM/leads/urls.py
@@ -6,7 +6,7 @@ app_name = 'leads'
 urlpatterns = [
     path('', views.lead_list, name="lead_list"),
     path('<int:pk>/', views.lead_detail, name="lead_detail"),
-    path('create/', views.lead_create, name="lead_create"),
+    path('<int:pk>/delete/', views.lead_delete, name="lead_delete"),
     path('<int:pk>/update/', views.lead_update, name="lead_update"),
-
+    path('create/', views.lead_create, name="lead_create"),
 ]
